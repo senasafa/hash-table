03ca47b7e2caedeb27050c4ca6297c5e74c38be2
senasafa <senasafa8@gmail.com>
2025-04-03 14:57:04 +0300

    Add files via upload

---
+++ hash table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Hash Table kullanarak Dictionary oluşturma
+        Dictionary<int, string> hashTable = new Dictionary<int, string>();
+
+        // Anahtar-Değer çiftlerini ekleme
+        hashTable.Add(1, "Sena");
+        hashTable.Add(2, "Yavuz");
+        hashTable.Add(3, "Burçin");
+
+        // Aranacak anahtar
+        int arananAnahtar = 2;
+
+        // Anahtarın olup olmadığını kontrol etme
+        if (hashTable.TryGetValue(arananAnahtar, out string bulunanDeger))
+        {
+            Console.WriteLine($"Anahtar {arananAnahtar} bulundu! Değer: {bulunanDeger}");
+        }
+        else
+        {
+            Console.WriteLine($"Anahtar {arananAnahtar} bulunamadı!");
+        }
+    }
+}
